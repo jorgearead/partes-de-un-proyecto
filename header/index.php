8d2c17dc1d8b3dc20b12b409c11448696ad36e71
netweb <netwebti@gmail.com>
2021-05-05 17:16:46 -0500

    Actualizacion

--- header/index.php
+++ header/index.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
-    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
     <script src="./js/index.js"></script>
     <title>Document</title>
@@ -27,5 +26,6 @@
         </nav>
         <section></section>
     </header>
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </body>
 </html>
